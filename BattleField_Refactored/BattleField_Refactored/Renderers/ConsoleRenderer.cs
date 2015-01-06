9c639453fe609ee412295d1356269bbef7dfd384
Yordan Darakchiev <iordan93@gmail.com>
2015-01-06 10:19:32 +0200

    Added console renderer and console input handler classes, TODO: implement them properly

---
+++ BattleField_Refactored/BattleField_Refactored/Renderers/ConsoleRenderer.cs
@@ -0,0 +1,21 @@
+﻿using BattleField_Refactored.Interfaces;
+namespace BattleField_Refactored.Renderers
+{
+    public class ConsoleRenderer : IRenderer
+    {
+        public void Render(IRenderable obj)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void RenderText(string text)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ BattleField_Refactored/BattleField_Refactored/UserInputHandlers/ConsoleUserInputHandler.cs
@@ -0,0 +1,21 @@
+﻿using BattleField_Refactored.Interfaces;
+namespace BattleField_Refactored.UserInputHandlers
+{
+    public class ConsoleUserInputHandler : IUserInputHandler
+    {
+        public int ReadInteger()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string ReadText()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IPosition ReadPosition()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
