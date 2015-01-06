6862d2b8abddf26248e5e2a08d5017d74a665d85
Yordan Darakchiev <iordan93@gmail.com>
2015-01-07 00:53:16 +0200

    Game objects structure change

---
+++ BattleField_Refactored/BattleField_Refactored/Common/MineType.cs
@@ -0,0 +1,11 @@
+﻿namespace BattleField_Refactored.Common
+{
+    public enum MineType
+    {
+        Mini = 1,
+        Double = 2,
+        Average = 3,
+        Big = 4,
+        Huge = 5
+    }
+}
---
+++ BattleField_Refactored/BattleField_Refactored/Interfaces/IMine.cs
@@ -0,0 +1,10 @@
+﻿using BattleField_Refactored.Common;
+namespace BattleField_Refactored.Interfaces
+{
+    public interface IMine : IExplosible
+    {
+        bool[,] Body;
+
+        MineType Type;
+    }
+}
